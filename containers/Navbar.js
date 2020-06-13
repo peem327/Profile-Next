78bdfc1bd7ee8be599c62cc0dabff9898d484bde
Peem <peem327@gmail.com>
2020-06-13 13:20:39 +0700

    update

--- containers/Navbar.js
+++ containers/Navbar.js
@@ -11,7 +11,7 @@ class Navbar extends React.Component {
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-          <a className="navbar-brand" href="/">I'm Peem</a>
+          <li className="nav-item nav-link link-color"> <Link href="/">I'm Peem</Link></li>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -19,7 +19,7 @@ class Navbar extends React.Component {
             <div className="navbar-nav ml-auto">
               <li className="nav-item nav-link link-color"><Link href="/">Home</Link></li>
               <li className="nav-item nav-link link-color"><Link href="/about">Profile</Link></li>
-              <li className="nav-item nav-link "><a https="https://forms.gle/3KuGuRGmrwrVFC9a6">Contact</a></li>
+              <li className="nav-item nav-link link-color"><a href="https://forms.gle/3KuGuRGmrwrVFC9a6">Contact</a></li>
             </div>
           </div>
         </nav>   
